9758b13e750dc9269d27f8de714a30411eaa969e
DmitryDubovikov <dmitry.dubovikov@gmail.com>
2023-06-07 11:32:34 -0300

    models added

--- iqeq/models.py
+++ iqeq/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class IQEQTest(models.Model):
+    login = models.CharField(max_length=10, unique=True)
+
+
+class IQTestResults(models.Model):
+    login = models.ForeignKey(IQEQTest, on_delete=models.CASCADE)
+    score = models.IntegerField()
+    time_taken = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"IQ test results of {self.login}"
+
+
+class EQTestResults(models.Model):
+    login = models.ForeignKey(IQEQTest, on_delete=models.CASCADE)
+    letters = models.CharField(max_length=5)
+    time_taken = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"EQ test results of {self.login}"
